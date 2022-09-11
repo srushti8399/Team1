93606305ffaa7b5f19d18585519f65ddeaf4b66a
miteshSakalkar77 <sakalkarmitesh77@gmail.com>
2022-09-11 14:05:02 +0530

    mockInterviewUI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MockComponent } from './mock/mock.component';
+import { ScheduleComponent } from './mock/schedule/schedule.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'mock', component:MockComponent},
+  {path:'mock/schedule', component:ScheduleComponent},
+{path:'mockInterview',
+  loadChildren:()=>import('./mock/mock.module').then(x=>x.MockModule)
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockModule } from './mock/mock.module';
 
 
 
@@ -17,7 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MockModule
   ],
   providers: [],
   bootstrap: [AppComponent]
