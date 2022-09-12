cbb87ac703907291b0e0f78e930f9b873bcb676b
miteshSakalkar77 <sakalkarmitesh77@gmail.com>
2022-09-12 16:16:39 +0530

    updateInUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from './auth/authservice/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AuthService]
 })
 export class AppComponent implements OnInit {
   title = 'ProjectA';
